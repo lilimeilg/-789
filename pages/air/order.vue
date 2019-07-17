8a8f35f96c7c6ec8f450af644586bb0f3ef1cfb1
lilimei <369667724@qq.com>
2019-07-17 19:55:25 +0800

    机票订单页显示

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -1,13 +1,36 @@
 <template>
-    <h1>订单页面</h1>
+    <div class="container">
+        <el-row type="flex" justify="space-between">
+            <!-- 订单表单 -->
+           <OrderForm />
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                    <h2>机票详情</h2>      
+            </div>
+        </el-row>
+    </div>
 </template>
 
 <script>
+import OrderForm from "@/components/air/orderForm"
 export default {
-
+    components:{
+       OrderForm 
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .container{
+        width:1000px;
+        margin:20px auto;
+    }
+    
+    /*aside*/
+    .aside{
+        width: 350px;
+        height: fit-content;
+        border:1px #ddd solid;
+    }
 </style>
